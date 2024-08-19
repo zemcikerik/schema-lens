86dfc3e4b5bc6f33272f55f227f341b849f52ad3
Erik Zemcik <zemcik.erik@gmail.com>
2024-08-19 17:51:40 +0200

    Add ng-mocks

--- ui/src/test-setup.ts
+++ ui/src/test-setup.ts
@@ -6,3 +6,16 @@ globalThis.ngJest = {
   },
 };
 import 'jest-preset-angular/setup-jest';
+
+import { ngMocks } from 'ng-mocks';
+import { MockService } from 'ng-mocks';
+import { CommonModule } from '@angular/common';
+import { ApplicationModule } from '@angular/core';
+import { DefaultTitleStrategy, TitleStrategy } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+ngMocks.autoSpy('jest');
+ngMocks.defaultMock(TitleStrategy, () => MockService(DefaultTitleStrategy));
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
