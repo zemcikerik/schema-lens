ab69c738f1f092564aa6b627b7c9c46157e5bee1
Erik Zemcik <zemcik.erik@gmail.com>
2024-08-20 17:58:25 +0200

    Remove tests created by project generation

--- ui/src/app/shared/components/code-editor/code-editor.component.spec.ts
+++ ui/src/app/shared/components/code-editor/code-editor.component.spec.ts
@@ -1,21 +1,3 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CodeEditorComponent } from './code-editor.component';
-
-describe('SqlEditorComponent', () => {
-  let component: CodeEditorComponent;
-  let fixture: ComponentFixture<CodeEditorComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CodeEditorComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(CodeEditorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+describe('CodeEditorComponent', () => {
+  it.todo('should be created');
 });
