f5115ee39fa7c39bf9d98be61105118be5830c16
Erik Zemčík <zemcik.erik@gmail.com>
2025-02-13 16:20:53 +0100

    Fix entity resize in negative direction

--- ui/src/app/diagrams/modules/entity-resize.module.ts
+++ ui/src/app/diagrams/modules/entity-resize.module.ts
@@ -1,8 +1,10 @@
+import type Canvas from 'diagram-js/lib/core/Canvas';
 import type EventBus from 'diagram-js/lib/core/EventBus';
 import type { ModuleDeclaration } from 'didi';
 import type { Shape } from 'diagram-js/lib/model/Types';
 import type { Dimensions, Rect } from 'diagram-js/lib/util/Types';
 import { EntityShape, isEntityElement } from '../shapes/entity.shape';
+import { translate } from 'diagram-js/lib/util/SvgTransformUtil';
 import AngularElementTrackerModule, { AngularElementTracker } from './angular-element-tracker.module';
 
 interface DraggingResizeStartEvent extends Event {
@@ -25,9 +27,13 @@ interface DraggingResizeCancelEvent extends Event {
 
 export class EntityResizeHandler {
 
-  static readonly $inject = ['eventBus', 'angularElementTracker'];
+  static readonly $inject = ['eventBus', 'canvas', 'angularElementTracker'];
 
-  constructor(eventBus: EventBus, private readonly angularElementTracker: AngularElementTracker) {
+  constructor(
+    eventBus: EventBus,
+    private readonly canvas: Canvas,
+    private readonly angularElementTracker: AngularElementTracker
+  ) {
     eventBus.on('resize.start', 1500, (event: DraggingResizeStartEvent) => {
       if (isEntityElement(event.shape)) {
         event.context.minDimensions = { ...event.shape.minDimensions };
@@ -36,19 +42,23 @@ export class EntityResizeHandler {
 
     eventBus.on('resize.move', 750, (event: DraggingResizeMoveEvent) => {
       if (isEntityElement(event.shape)) {
-        this.updateWrapperDimensions(event.shape, event.context.newBounds);
+        this.updateWrapperRectangle(event.shape, event.context.newBounds);
         event.stopPropagation();
       }
     });
 
     eventBus.on('resize.cancel', (event: DraggingResizeCancelEvent) => {
       if (isEntityElement(event.shape)) {
-        this.updateWrapperDimensions(event.shape, event.shape);
+        this.updateWrapperRectangle(event.shape, event.shape);
       }
     });
   }
 
-  private updateWrapperDimensions(entity: EntityShape, { width, height }: Dimensions) {
+  private updateWrapperRectangle(entity: EntityShape, { x, y, width, height }: Rect): void {
+    // translate graphics manually to prevent needles redraws
+    const graphics = this.canvas.getGraphics(entity);
+    translate(graphics, x, y);
+
     const wrapper = this.angularElementTracker.getComponentWrapper(entity);
     wrapper?.setAttribute('width', String(width));
     wrapper?.setAttribute('height', String(height));
