7dba188ecd572c404463eaf591da75c0bcaa439a
Erik Zemčík <zemcik.erik@gmail.com>
2025-02-24 19:49:56 +0100

    Ensure username and email uniqueness ignoring case on DB level, improve query performance

--- api/src/main/kotlin/dev/zemco/schemalens/auth/UserRepository.kt
+++ api/src/main/kotlin/dev/zemco/schemalens/auth/UserRepository.kt
@@ -1,10 +1,15 @@
 package dev.zemco.schemalens.auth
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository : CrudRepository<User, Long> {
+    @Query("SELECT CASE WHEN EXISTS (SELECT 'X' FROM dev.zemco.schemalens.auth.User WHERE LOWER(username) = LOWER(:username)) THEN TRUE ELSE FALSE END")
     fun existsByUsernameIgnoreCase(username: String): Boolean
+
+    @Query("SELECT CASE WHEN EXISTS (SELECT 'X' FROM dev.zemco.schemalens.auth.User WHERE LOWER(email) = LOWER(:email)) THEN TRUE ELSE FALSE END")
     fun existsByEmailIgnoreCase(email: String): Boolean
 
+    @Query("SELECT u FROM dev.zemco.schemalens.auth.User u WHERE LOWER(u.username) = LOWER(:username)")
     fun findByUsernameIgnoreCase(username: String): User?
 }
