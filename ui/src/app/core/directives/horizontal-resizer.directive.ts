f50adf06c482d138d5582eb7500a7b9440d6f713
Erik Zemčík <zemcik.erik@gmail.com>
2026-01-25 14:53:36 +0100

    Add sidebar resizing and reversing

---
+++ ui/src/app/core/directives/horizontal-resizer.directive.ts
@@ -0,0 +1,37 @@
+import { DestroyRef, Directive, DOCUMENT, inject, input } from '@angular/core';
+import { fromEvent } from 'rxjs';
+
+export interface HorizontalResizerTarget {
+  getWidth(): number;
+  setWidth(width: number): void;
+}
+
+@Directive({
+  selector: '[appHorizontalResizer]',
+  host: {
+    '(mousedown)': 'handleDrag($event)',
+  },
+})
+export class HorizontalResizerDirective {
+  resizerTarget = input.required<HorizontalResizerTarget>();
+  resizerReverse = input<boolean>(false);
+
+  private document = inject(DOCUMENT);
+  private destroyRef = inject(DestroyRef);
+
+  handleDrag(event: MouseEvent): void {
+    const startWidth = this.resizerTarget().getWidth();
+    const startX = event.clientX;
+    const direction = this.resizerReverse() ? -1 : 1;
+
+    const subscription = fromEvent(this.document, 'mousemove').subscribe(event => {
+      this.resizerTarget().setWidth(Math.max(startWidth + direction * ((event as MouseEvent).clientX - startX), 0));
+    });
+
+    subscription.add(fromEvent(this.document, 'mouseup').subscribe(() => {
+      subscription.unsubscribe();
+    }));
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
--- ui/src/app/core/layouts/layout-content-with-sidebar.component.ts
+++ ui/src/app/core/layouts/layout-content-with-sidebar.component.ts
@@ -1,21 +1,37 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnDestroy, OnInit } from '@angular/core';
 import { SidebarStateService } from './sidebar-state.service';
+import { HorizontalResizerDirective } from '../directives/horizontal-resizer.directive';
+import { SidebarResizeTargetDirective } from './sidebar-resize-target.directive';
 
 @Component({
   selector: 'app-layout-content-with-sidebar',
   template: `
-    <div class="layout-content-with-sidebar" [class.is-open]="sidebarState.isOpen()">
-      <aside class="layout-content-with-sidebar__sidebar">
+    <div #layoutRoot class="layout-content-with-sidebar" [class.is-open]="sidebarState.isOpen()" [class.reverse]="reverse()">
+      <aside
+        class="layout-content-with-sidebar__sidebar"
+        appSidebarResizeTarget
+        [widthCssProperty]="'--layout-sidebar-width'"
+        [widthCssPropertyTarget]="layoutRoot"
+        #sidebarResizeTarget="sidebarResizeTarget"
+      >
         <ng-content select="sidebar" />
       </aside>
+      <div
+        class="layout-content-with-sidebar__resizer"
+        appHorizontalResizer
+        [resizerTarget]="sidebarResizeTarget"
+        [resizerReverse]="reverse()"
+      ></div>
       <div class="layout-content-with-sidebar__content">
         <ng-content />
       </div>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [HorizontalResizerDirective, SidebarResizeTargetDirective],
 })
 export class LayoutContentWithSidebarComponent implements OnInit, OnDestroy {
+  reverse = input<boolean>(false);
   sidebarState = inject(SidebarStateService);
 
   ngOnInit(): void {
