e23d2a8d53f8118c42c1f4e2e4cb5e7e37c7c65b
Erik Zemčík <zemcik.erik@gmail.com>
2026-04-30 21:09:18 +0200

    Add owner column to data model list table with user profile picture

--- ui/src/app/data-models/components/data-model-list-table/data-model-list-table.component.ts
+++ ui/src/app/data-models/components/data-model-list-table/data-model-list-table.component.ts
@@ -1,18 +1,22 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, output } from '@angular/core';
 import { DataModel } from '../../models/data-model.model';
 import { MatTableModule } from '@angular/material/table';
 import { MatRipple } from '@angular/material/core';
 import { TranslatePipe } from '../../../core/translate/translate.pipe';
+import { ProfilePictureComponent } from '../../../shared/components/profile-picture/profile-picture.component';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-data-model-list-table',
   templateUrl: './data-model-list-table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatTableModule, MatRipple, TranslatePipe],
+  imports: [MatTableModule, MatRipple, TranslatePipe, ProfilePictureComponent],
 })
 export class DataModelListTableComponent {
-  readonly DISPLAYED_COLUMNS = ['name'];
+  readonly DISPLAYED_COLUMNS = ['name', 'owner'];
 
   dataModels = input.required<DataModel[]>();
   selected = output<DataModel>();
+
+  currentUser = inject(AuthService).currentUser;
 }
