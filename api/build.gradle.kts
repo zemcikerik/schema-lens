0c9ea49e811162a24e9e0a883879e1665f7132bf
Erik Zemčík <zemcik.erik@gmail.com>
2026-05-02 09:51:58 +0200

    Update dependencies to latest versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
-    kotlin("plugin.spring") version "2.3.20"
-    kotlin("plugin.jpa") version "2.3.20"
-    kotlin("plugin.allopen") version "2.3.20"
-    id("org.springframework.boot") version "4.0.3"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.3.21"
+    kotlin("plugin.jpa") version "2.3.21"
+    kotlin("plugin.allopen") version "2.3.21"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
